4e98682eba78c18d016fc39444554ac2e2325955
AbdulWahabRaza123 <abdulwahabraza123@gmail.com>
2023-03-21 01:23:48 +0500

    set the path for radiologist for storing report

--- server/src/router/auth.js
+++ server/src/router/auth.js
@@ -254,7 +254,8 @@ router.post("/login", async (req, res) => {
 });
 router.post("/GenerateReport",upload.single("Image"),async(req,res)=>{
   try {
-    console.log("I am here ");
+    const {email}=req.body;
+    
     res.status(200).json({ message: "done", report: "This is report from backend..." });
   }catch(e){
     return res.status(401).json({ message: "error", type: "unknown error" });
